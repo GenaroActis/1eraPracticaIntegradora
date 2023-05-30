6d62a6509ed844bd333ceb82836e8e98bd5f821f
GenaroActis <113357267+GenaroActis@users.noreply.github.com>
2023-05-30 17:37:07 -0300

    update

---
+++ app/src/daos/mongodb/messagesDao.js
@@ -0,0 +1,36 @@
+import {MessagesModel} from './models/messagesModel.js'
+
+export default class MessagesDaoDB{
+    async getAllMessages(){
+        try {
+            const response = await MessagesModel.find({})
+            return response
+        } catch (error) {
+            console.log(error)
+        };
+    };
+    async sendMessage(usName, message) {
+        try {
+            const newMessage = await MessagesModel.create({userName: usName, msg: message})
+            return newMessage
+        } catch (error) {
+            console.log(error)
+        };
+    };
+    async editMessage(msgId, obj) {
+        try {
+        const updMessage = await MessagesModel.updateOne({_id: msgId}, obj);
+        return updMessage;
+        } catch (error) {
+            console.log(error)
+        };
+    };
+    async deleteMessage(msgId) {
+        try {
+            const delMessage = await MessagesModel.findByIdAndDelete(msgId)
+            return delMessage
+        } catch (error) {
+            console.log(error)
+        };
+    };
+};
